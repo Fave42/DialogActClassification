185b6dfd8ceb71f9ca9d30efef09941372f4b560
Fabian <fabian.fey@ims.uni-stuttgart.de>
2018-01-15 12:00:22 +0100

    Löschung aller Ausgaben für die Evaluation des Scripts und dadurch unnötig gewordenen Codezeilen. Abspeichern aller für TensorFlow benötigter Daten mit Pickle. Bereinigen des Codes und des Ordners.

--- Data_PreProcessing/generateTrainingData_onlyLexical.py
+++ Data_PreProcessing/generateTrainingData_onlyLexical.py
@@ -12,84 +12,111 @@ import sys
 import gensim
 import time # To time the import and excecution
 import operator # For sorting the dictionary that is storing the unknown words
+import random as rand
+import pickle
 
 #fileName = sys.argv[1]
-# For testing provide the file "TestDict.txt" wich is provided in the parent folder of this script
-fileName = "F:/Mega/Uni-Master/Deep Learning/Projekt/Data/Train/train.txt"
+# Fabian-Laptop Path
+#fileName = "D:/Mega/Uni-Master/Deep Learning/Projekt/Data/Train/train.txt"
+# Serverpath
+#fileName = "/mount/arbeitsdaten31/studenten1/deeplearning/2017/Deep_Learners/Data/Train/train.txt"
+#fileName = "/mount/arbeitsdaten31/studenten1/deeplearning/2017/Deep_Learners/Data/Dev/dev.txt"
+fileName = "/mount/arbeitsdaten31/studenten1/deeplearning/2017/Deep_Learners/Data/Test/test.txt"
+
+# Select the padding depth of the matrix (1=one line with 0's, 2=two lines with 0's
+paddingDepth = 1
+
+# Loads "unknownWordsDict.txt that all every unknown words has unique 300 random float numbers
+unknownWordsDict = {}
+# Fabian-Laptop Path
+#unknownWordsDict = pickle.load(open("unknownWordsDict.pickle", "rb"))
+# Serverpath
+unknownWordsDict = pickle.load(open("dict/unknownWordsDict.pickle", "rb"))
+
+# Stores every sentence to be dumped with pickle
+outputList = []
+
+# Generates 300 random numbers between -1 and 1 for unknown words
+def getRandomDimensions():
+    randomList = []
+    for i in range(0,300):
+        randomList.append(rand.uniform(-1.0, 1.0))
+    return randomList
 
 # Start timing the importing of the dictionary
 startTime = time.time()
 
 # Load Google's pre-trained Word2Vec model.
-# Richie Laptop
-#model = gensim.models.KeyedVectors.load_word2vec_format(
-#    '/home/richard/Projekte/Project_Deep_Learners/Processing_Resources/dict/GoogleNews-vectors-negative300.bin',
-#    binary=True)
+# Laptop/PC Verions
+# model = gensim.models.KeyedVectors.load_word2vec_format(
+#     'dict/GoogleNews-vectors-negative300.bin', binary=True)
+# Server Version
+model = gensim.models.Word2Vec.load_word2vec_format(
+    'dict/GoogleNews-vectors-negative300.bin', binary=True)
+# Path Richie: '/home/richard/Projekte/Project_Deep_Learners/Processing_Resources/dict/GoogleNews-vectors-negative300.bin'
 
-# Fabian Laptop
-model = gensim.models.KeyedVectors.load_word2vec_format(
-    'D:/word2vec_Test/dict/GoogleNews-vectors-negative300.bin', binary=True)
 endTime = time.time()
 
 print("--- Dictionary was imported successfully! ---")
 
-# Generate the outputfilfe wich is stored in the root folder
-outputFile = open("testOutput.txt", "w")
-
-# Count and keep the unknown words for later review
-countKnown = 0
-countUnknown = 0
-unknownWordsDict = {}
-
-
 # Start timing the creation of the training set
 startTime2 = time.time()
 
-with open(fileName, "r") as dictionary:
-    for line in dictionary:
+with open(fileName, "r") as dataFile:
+    for line in dataFile:
 
         splittedLine = line.split()
         fileID = splittedLine[0]
         diagClass = splittedLine[1]
 
+        # first list stores the actual featureMatrix, second tuple stores the one-hot vector.
+        trainingTuple = [[], []]
+        for i in range(0, paddingDepth):
+            zeroPadding = [0] * 300
+            trainingTuple[0].append(zeroPadding)
+
+        #print(trainingTuple[0])
+
         for i in range(2, len(splittedLine)):
-            # first list stores the actual featureMatrix, second tuple stores the one-hot vector.
-            trainingTuple = [[], []]
             word = splittedLine[i]
             # Check if the word is in the model
             # If not fill the list with 300 0's
             if (splittedLine[i] in model.vocab):
                 word300 = model[word]
-                countKnown += 1
+                word300 = word300.tolist()
+            elif (splittedLine[i] in unknownWordsDict):
+                word300 = unknownWordsDict[word]
             else:
-                word300 = [0] * 300
-                countUnknown += 1
-                if (word not in unknownWordsDict):
-                    unknownWordsDict[word] = 1
-                else:
-                    unknownWordsDict[word] += 1
+                word300 = getRandomDimensions()
+                unknownWordsDict[word] = word300
+
             trainingTuple[0].append(word300)
 
-            if diagClass == "backchannel":
-                trainingTuple[1] = [1, 0, 0, 0]
-            elif diagClass == "statement":
-                trainingTuple[1] = [0, 1, 0, 0]
-            elif diagClass == "question":
-                trainingTuple[1] = [0, 0, 1, 0]
-            elif diagClass == "opinion":
-                trainingTuple[1] = [0, 0, 1, 0]
+        for i in range(0, paddingDepth):
+            zeroPadding = [0] * 300
+            trainingTuple[0].append(zeroPadding)
 
-            #print('OK')
-            # Store the tuple in an output file
-            # Each tuple/word is currently in one line
-            outputFile.write((word) + " ")
-            for item in trainingTuple[0]:
-                for subitem in item:
-                    outputFile.write(str(subitem) + " ")
-            for item in trainingTuple[1]:
-                outputFile.write(str(item) + " ")
-            outputFile.write("\n")
-outputFile.close()
+        if diagClass == "backchannel":
+            trainingTuple[1] = [1, 0, 0, 0]
+        elif diagClass == "statement":
+            trainingTuple[1] = [0, 1, 0, 0]
+        elif diagClass == "question":
+            trainingTuple[1] = [0, 0, 1, 0]
+        elif diagClass == "opinion":
+            trainingTuple[1] = [0, 0, 0, 1]
+
+        #print("################")
+        #print(trainingTuple)
+
+        outputList.append(trainingTuple)
+
+# Saves the unknown words with their respective 300 random numbers
+with open('dict/unknownWordsDict.pickle', 'wb') as handle:
+    pickle.dump(unknownWordsDict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# Saves the the complete list with every sentence
+with open('Output_Files/testExamples_2WordContext.pickle', 'wb') as handle:
+    pickle.dump(outputList, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 endTime2 = time.time()
 duration = endTime - startTime
@@ -97,11 +124,3 @@ duration2 = endTime2 - startTime2
 
 print("Import duration of dictionary was " + str(duration) + " seconds!")
 print("The rest of the program was completed in " + str(duration2) + " seconds!")
-print(str(countKnown) + " words where found in the dictionary, " + str(countUnknown) + " weren't.")
-print("The unknown words are stored in the file Unknown_Words.txt.")
-
-# Create a sorted file with all unknown words their frequency
-sortedUnknownWordsList = sorted(unknownWordsDict.items(), key=operator.itemgetter(1)) # Is saved as a list of Tuples
-with open("Unknown_Words.txt", "w") as unknownWordsFile:
-    for item in reversed(sortedUnknownWordsList):
-        unknownWordsFile.write(str(item[0]) + " " + str(item[1]) + "\n")
