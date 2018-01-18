f568c5cf0293c710b370ac63615db2b6b4b2b38c
Fabian <fabian.fey@ims.uni-stuttgart.de>
2018-01-18 12:54:00 +0100

    Kleine Änderungen in CNN.py

--- DialogAct-CNN/CNN.py
+++ DialogAct-CNN/CNN.py
@@ -8,7 +8,8 @@
 The script is stored on the server under the following path:
 /mount/arbeitsdaten31/studenten1/deeplearning/2017/Deep_Learners/Processing_Resources
 
-Usage: python3 CNN.py
+Usage: python2.7 CNN.py
+!!!!!!!!! Server currently only supports TensorFlow python2.7 !!!!!!!!!
 '''
 
 ### Imports ###
@@ -17,26 +18,30 @@ import numpy as np
 import pickle
 
 # Server Paths
-# pathTraining = "NN_Input_Files/trainingExamples_2WordContext.pickle"
-# pathEvaluation = "NN_Input_Files/devExamples_2WordContext.pickle"
+#pathTraining = "NN_Input_Files/trainData_3-5WordContext_prot2.pickle"
+#pathEvaluation = "NN_Input_Files/devData_3-5WordContext_prot2.pickle"
 
 # PC Paths
-pathTraining = "D:/NN_Projekt/trainingExamples_2WordContext.pickle"
-pathEvaluation = "D:/NN_Projekt/devExamples_2WordContext.pickle"
+pathTraining = "D:/NN_Projekt/trainData_3-5WordContext.pickle"
+pathEvaluation = "D:/NN_Projekt/devData_3-5WordContext.pickle"
 
+#trainingList = pickle.load(open(pathTraining, "rb"))
+evaluationList = pickle.load(open(pathEvaluation, "rb"))
 
-trainingList = pickle.load(open(pathTraining, "rb"))
-#evaluationList = pickle.load(open(pathEvaluation, "rb"))
-
-trainingArray = np.asarray(pathTraining)
+#trainingArray = np.asarray(pathTraining)
 #evaluationArray = np.asarray(evaluationList)
 
-print(trainingArray[0, 0].shape)
-print(trainingArray[0])
+print(evaluationList[0, 0].shape)
+print(evaluationList[0])
+
+evalDataTF = tf.convert_to_tensor(evaluationList, np.float32)
+
+print(evalDataTF[0, 0].shape)
+print(evalDataTF[0])
 
 features = []
 labels = []
-for item in trainingArray:
+for item in evalDataTF:
     features.append(item[0])
     labels.append(item[1])
 
@@ -53,8 +58,6 @@ labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
 
 dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, labels_placeholder))
 iterator = dataset.make_initializable_iterator()
-
-
 
 ### Functions ###
 def weightVariable(shape):
@@ -101,7 +104,7 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 # Assigning values to placeholders and running the graph
-input = trainingArray[0, 0]
+input = evalDataTF[0, 0]
 
 # we want to get (fetch) the value of a, feeding the input vector for the placeholders
 output = sess.run([tmp], {x: input})
